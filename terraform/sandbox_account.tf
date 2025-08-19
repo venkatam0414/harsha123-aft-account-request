7961c478cf4c2fcb360cd7a94ab8d7cdf46daf8a
venkatam0414 <venkata.m0414@gmail.com>
2025-08-19 02:17:14 -0400

    Update sandbox_account.tf

--- terraform/sandbox_account.tf
+++ terraform/sandbox_account.tf
@@ -1,21 +1,18 @@
-// harsha123-aft-account-request/terraform/sandbox_account.tf
-
 terraform {
   required_version = ">= 1.5.0"
 }
 
-// One account request = one module block
 module "sandbox_dev_001" {
-  // Pin to a recent AFT release
-  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory//modules/aft-account-request?ref=v1.15.1"
+  source  = "aws-ia/control_tower_account_factory//modules/aft-account-request"
+  version = ">= 1.12.0"
 
   control_tower_parameters = {
-    AccountEmail               = "venkata.m0414+sandbox-dev@gmail.com"  // must be unique in AWS
-    AccountName                = "Sandbox-Dev-001"
-    ManagedOrganizationalUnit  = "Sandbox"   // must match your existing OU name exactly
-    SSOUserEmail               = "venkata.m0414+admin@gmail.com"
-    SSOUserFirstName           = "Admin"
-    SSOUserLastName            = "User"
+    AccountEmail              = "venkata.m0414+sandbox-dev@gmail.com"
+    AccountName               = "Sandbox-Dev-001"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "venkata.m0414+admin@gmail.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
   }
 
   account_tags = {
